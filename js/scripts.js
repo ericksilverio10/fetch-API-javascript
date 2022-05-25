2c9a9fd113a88d6a1d50b55e87a7cad35b86f3f7
Erick <erick1junior@hotmail.com>
2022-05-25 13:26:15 -0300

    Configurando script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log('Olá mundo');
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+
+// Get all posts
+async function getAllPosts() {
+    const response = await fetch(url);
+    console.log(response);
+}
+
+getAllPosts();
